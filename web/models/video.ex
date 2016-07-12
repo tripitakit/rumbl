0fcfdb8a4144522429701656f121da6276679768
tripitakit <patrick.demarta@gmail.com>
2016-07-12 19:38:55 +0200

    persisted annotation, show history on join

--- web/models/video.ex
+++ web/models/video.ex
@@ -8,6 +8,7 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     field :slug, :string
+    has_many :annotations, Rumbl.Annotation
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
